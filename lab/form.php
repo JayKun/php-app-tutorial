38ee4c62de0c5f2cdeac20b68e775dc5bddb0b57
Jun Kai <junkaiong11@gmail.com>
2017-08-23 10:32:58 +0800

    Update form.php

--- lab/form.php
+++ lab/form.php
@@ -1,35 +1,32 @@
 <?php
-        //insert appropriately based on your MySQL database information
-		$servername = "";
-		$username = "";
-		$password = "";
-		$dbname = "";
+        // TODO: Fill up the necessary database credentials based on your MySQL database information
+	$servername = "";
+	$username = "";
+	$password = "";
+	$dbname = "";
 
-		// Create connection
-        // mysqli is an inbuilt library class
-		$conn = new mysqli($servername, $username, $password, $dbname);
-		
-		// Check connection
-		if ($conn->connect_error)
-        {
-		    die("Connection failed: " . $conn->connect_error);
-		} 
-    
-    
-		$sql = "INSERT INTO students(firstname, lastname, email)
-		VALUES ('".$_POST['firstname']. "', '" .$_POST['lastname']. "','" .$_POST['email']."')";
+	// Initialize connection with the students database
+	// mysqli is an inbuilt library class
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-		if ($conn->query($sql) === TRUE)
-        {
-		    echo "New record created successfully";
-		}
-    
-        else
-        {
-		    echo "Error: " . $sql . "<br>" . $conn->error;
-		}
+	// Check for connection errors
+	if ($conn->connect_error){
+	    die("Connection failed: " . $conn->connect_error);
+	} 
 
-		$conn->close();
+        // TODO: Construct a SQL query that inserts an entry to the students table.
+	$sql = "INSERT INTO students()
+	VALUES ('".$_POST['firstname']. "', '" .$_POST['lastname']. "','" .$_POST['email']."')";
+
+	if ($conn->query($sql) === TRUE){
+	    echo "New record created successfully";
+	}
+
+	else{
+	    echo "Error: " . $sql . "<br>" . $conn->error;
+	}
+
+	$conn->close();
 ?>
 
 <!DOCTYPE html>
